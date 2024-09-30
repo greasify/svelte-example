9553a0d254ee4f3e58e1140fb58edb4c1b89cdea
crashmax-dev <crashmax.dev@gmail.com>
2024-09-30 05:58:48 +0000

    Deploying to gh-pages from @ greasify/svelte-example@7676b10705c52288a48fffd70c4972fbfcc80836 🚀

---
+++ greasify-svelte.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        greasify-svelte
+// @version     0.0.0
+// @homepage    https://greasify.github.io/svelte-example/
+// @match       https://example.com
+// @resource    style https://greasify.github.io/svelte-example/style.css?ts=1727675927468
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// @updateURL   https://greasify.github.io/svelte-example/greasify-svelte.meta.js
+// @downloadURL https://greasify.github.io/svelte-example/greasify-svelte.user.js
+// ==/UserScript==
